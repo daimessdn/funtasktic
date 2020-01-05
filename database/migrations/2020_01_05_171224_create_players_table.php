5c9e14194363b314eeede28c08cf223f6baa1d65
daimessdn <daimessdn@gmail.com>
2020-01-06 00:38:24 +0700

    created player migration db

---
+++ database/migrations/2020_01_05_171224_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migratio ns.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('level')->default(1);
+            $table->integer('health')->default(100);
+            $table->integer('max_health')->default(100);
+            $table->integer('xp')->default(1);
+            $table->integer('max_xp')->default(100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('player', function (Blueprint $table) {
+            //
+        });
+    }
+}
