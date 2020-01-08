90469dffc6d16b3f53f923ea8490373457f5db52
daimessdn <daimessdn@gmail.com>
2020-01-08 22:03:15 +0700

    create relation of user-player

--- app/Player.php
+++ app/Player.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\User;
 
 class Player extends Model
 {
     protected $table = 'player';
+
+    public function player () {
+    	return $this->belongsTo(User::class);
+    }
 }
