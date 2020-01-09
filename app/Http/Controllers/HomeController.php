53d3cb1a9b36676560d3c84d064121b46290c29c
daimessdn <daimessdn@gmail.com>
2020-01-09 19:30:11 +0700

    sort tasks based on due date

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index() {
     	$user = Auth::user();
 
-    	return view('home')->with('user', $user)->with('player', $user->player)->with('tasks', $user->player->task->where('completed', '=', 0));
+    	return view('home')->with('user', $user)->with('player', $user->player)->with('tasks', $user->player->task->where('completed', '=', 0)->sortBy('due'));
     }
 
     public function logout() {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,12 @@
 	<meta name=description content="">
 	<meta name=viewport content="width=device-width, initial-scale=1">
 	<title>funtasktic</title>
+	<link href="https://fonts.googleapis.com/css?family=DM+Sans&display=swap" rel="stylesheet"> 
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<style type="text/css" media="screen">
 		body, html, * {
-			font-family: 'Helvetica Neue';
+			font-family: 'DM Sans';
 		}
 
 		i.fa {
@@ -108,7 +109,7 @@
 				<td class="col-md-6">
 					<strong>{{ $task->task_name }}</strong><br />
 					@if ($task->task_desc)
-						{{ $task->task_desc }}<br />
+						{{ $task->task_desc }}
 					@endif
 					<span class="badge badge-primary">{{ $task->due }}</span>
 				</td>
