6fbd81b201bcbfb325072f9236e0c4421f65ea79
daimessdn <daimessdn@gmail.com>
2020-01-14 09:45:18 +0700

    heading again :v

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -23,9 +23,7 @@ class UsersExport implements FromCollection, WithHeadings
             'id',
             'name',
             'email',
-            'email_verified_at',
             'password',
-            'remember_token',
             'created_at',
             'updated_at'
         ];
