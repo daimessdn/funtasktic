c18a29eef9220edc88756ebcd5d422127f50fe2a
daimessdn <daimessdn@gmail.com>
2020-02-01 16:40:45 +0700

    added table for notifications

---
+++ database/migrations/2020_02_01_160806_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('player_id');
+            $table->integer('type');
+            $table->string('notification_content');
+            $table->integer('dismissed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
